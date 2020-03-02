a66dc084d4c0a2a8f71cff4b4e8c78529cc8c8b8
annprog <tecbbs@qq.com>
2020-03-02 13:55:02 +0800

    [ci skip] fix alpine repositories

--- ghc.sh
+++ ghc.sh
@@ -2,23 +2,12 @@
 
 set -e
 
-function _repo() {
-	cat > /etc/apk/repositories <<EOF
-http://mirrors.ustc.edu.cn/alpine/v3.9/main
-http://mirrors.ustc.edu.cn/alpine/v3.9/community
-EOF
-}
-
 function _repoEdge() {
-		cat > /etc/apk/repositories <<EOF
-http://mirrors.ustc.edu.cn/alpine/v3.9/main
-http://mirrors.ustc.edu.cn/alpine/v3.9/community
-http://mirrors.ustc.edu.cn/alpine/edge/main
-http://mirrors.ustc.edu.cn/alpine/edge/community
+		cat >> /etc/apk/repositories <<EOF
+http://dl-cdn.alpinelinux.org/alpine/edge/main
+http://dl-cdn.alpinelinux.org/alpine/edge/community
 EOF
 }
-
-_repo
 
 apk update
 apk add make libc-dev pcre-dev libc6-compat ncurses5-libs gmp-dev llvm zlib-dev gcc perl g++ git aria2 xz
@@ -34,6 +23,7 @@ if [ "$ARCH"x == "aarch64"x ];then
 	_repoEdge
 	apk update
 	apk add numactl-dev
+	sed -i '/edge/d' /etc/apk/repositories
 
 	cd /ghc-8.6.2
    ./configure
